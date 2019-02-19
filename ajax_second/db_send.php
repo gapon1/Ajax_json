daee1fae78cea057b2f911d952708760d4c80129
Vitaliy <gapon007@ukr.net>
2019-02-19 16:41:46 +0200

    Create new project for ajax tutorial ADD DATA TO DB USE AJAX

---
+++ ajax_second/db_send.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pro
+ * Date: 2019-02-19
+ * Time: 16:40
+ */
---
+++ ajax_second/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+<form action="" method="post">
+    <input type="text" name="name" placeholder="name">
+    <input type="text" name="lastname" placeholder="lastname">
+    <input type="number" name="age" placeholder="age">
+
+    <button type="submit">Send</button>
+
+
+</form>
+
+
+</body>
+</html>
